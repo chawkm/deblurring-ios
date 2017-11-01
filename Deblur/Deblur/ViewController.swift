28f4f032d9dd35787bcc399ca58a47e8a035c72f
Chris Hawkes <chawkesmexico@gmail.com>
2017-11-01 11:23:08 +0000

    [CH] First commit.

---
+++ Deblur/Deblur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deblur
+//
+//  Created by Christopher Hawkes on 01/11/2017.
+//  Copyright © 2017 Group 8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
